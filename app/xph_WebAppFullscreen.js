3bbd56b2b04499a263ff7417d0685743143ef57b
Nikita <nikitaskv0618@gmail.com>
2023-08-21 19:46:32 +0700

    * rename folder "html5game" -> "app" + added fullscreen button

---
+++ app/xph_WebAppFullscreen.js
@@ -0,0 +1,24 @@
+function __web_app_get_fullscreen() {
+	return !((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen));
+}
+
+function __web_app_set_fullscreen(enable) {
+	if(enable) {
+		if (document.documentElement.requestFullScreen) {
+			document.documentElement.requestFullScreen();
+		} else if (document.documentElement.mozRequestFullScreen) {
+			document.documentElement.mozRequestFullScreen();
+		} else if (document.documentElement.webkitRequestFullScreen) {
+			document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+		}
+	} else {
+		if (document.cancelFullScreen) {
+			document.cancelFullScreen();
+		} else if (document.mozCancelFullScreen) {
+			document.mozCancelFullScreen();
+		} else if (document.webkitCancelFullScreen) {
+			document.webkitCancelFullScreen();
+		}
+	}
+}
+
